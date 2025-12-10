e24092fa1f2b632bb199936c605ca245ebcf6c45
Dinesh <pydinesh3@gmail.com>
2025-12-10 22:36:24 +0530

    Update codeShare2.sql

--- VBCS/Component/SinglSelect/codeShare2.sql
+++ VBCS/Component/SinglSelect/codeShare2.sql
@@ -1,1 +1,13 @@
+<div>
+    <oj-select-single style="width: 18rem;" value="{{ $variables.reProcessObj.LegalEntity }}" label-hint="Legal Entity"
+      data="[[$variables.getLegalEntityListSDP2]]" item-text="legal_entity" id="legid" ></oj-select-single>
+    <span class="oj-ux-ico-information" on-click="[[$listeners.iconClick]]"></span>    
+</div>
 <oj-highlight-text style="color: red;" text="[[$application.translations.appBundle.msg]]" match-text="Highlight"></oj-highlight-text>
+
+
+
+<!-- DELETE DIALOG -->
+<oj-popup id="popDialog" tail="simple" position.at.vertical="top" modality="modal">
+ <oj-highlight-text style="color: red;" text="[[$application.translations.appBundle.msg]]" match-text="Highlight"></oj-highlight-text>
+</oj-popup>
